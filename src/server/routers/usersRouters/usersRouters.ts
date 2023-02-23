f8bd604611636a3f0c012478016d190ce4ba9578
Victor Granda <victorgranda1992@gmail.com>
2023-02-24 00:20:06 +0100

    Add usersRouter, implement multer and registerSchema

--- src/server/routers/usersRouters/usersRouters.ts
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -1,9 +1,30 @@
+import { validate } from "express-validation";
 import { Router } from "express";
-import { createUser, loginUser } from "../../controllers/usersControllers";
+import multer from "multer";
+import { uuid } from "uuidv4";
+import { createUser, loginUser } from "../../controllers/usersControllers.js";
+import registerSchema from "../schemas/registerSchemas.js";
 
 const usersRouter = Router();
 
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename(req, file, callback) {
+    const fileNamePrefix = Date.now();
+    const fileNameSuffix = uuid();
+    callback(null, `${fileNamePrefix}-${file.originalname}`);
+  },
+});
+
+export const upload = multer({ storage });
+const avatarUpload = upload.single("avatar");
+
 usersRouter.post("/login", loginUser);
-usersRouter.post("/register", createUser);
+usersRouter.post(
+  "/register",
+  avatarUpload,
+  validate(registerSchema),
+  createUser
+);
 
 export default usersRouter;
